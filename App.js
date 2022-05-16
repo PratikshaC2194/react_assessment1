ec788135a61d4955e67a446be4cc0da1848dd615
PratikshaC2194 <97968229+PratikshaC2194@users.noreply.github.com>
2022-05-16 12:07:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Preview from './Preview';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Preview/>
+    
+      
+    </div>
+  );
+}
+
+export default App;
